765a8a243d5d781fb4c0ed914d0e3973946f9a33
Lacey <lacey.griffith@g.austincc.edu>
2021-06-17 14:18:32 -0500

    implement indexdb to allow transactions to be saved while offline and sent to db when back online

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -10,7 +10,7 @@ request.onsuccess = function(event){
     db = event.target.result;
     
     if(navigator.onLine){
-        //uploadTransactions();
+        uploadTransactions();
     }
 }
 
@@ -26,3 +26,39 @@ function saveRecord(record){
     transactionObjectStore.add(record)
 }
 
+function uploadTransactions() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function(){
+        if(getAll.result.length > 0){
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type':'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(serverResponse => {
+                if(serverResponse.message){
+                    throw new Error(serverResponse)
+                }
+
+                const transaction = db.transaction(['new_transaction'], 'readwrite')
+                const transactionObjectStore = transaction.objectStore('new_transaction')
+                transactionObjectStore.clear();
+                alert('All saved transactions have been submitted!')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransactions)
