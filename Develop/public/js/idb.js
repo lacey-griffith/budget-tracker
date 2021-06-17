c406666b1ce5a7a21254f9b99a6eb3028b781a64
Lacey <lacey.griffith@g.austincc.edu>
2021-06-17 13:09:06 -0500

    connect to indexdb

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,28 @@
+let db;
+const request = indexedDB.open('budget-app', 1)
+
+request.onupgradeneeded = function(event){
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true })
+}
+
+request.onsuccess = function(event){
+    db = event.target.result;
+    
+    if(navigator.onLine){
+        //uploadTransactions();
+    }
+}
+
+request.onerror = function(event){
+    console.log(event.target.errorCode)
+}
+
+function saveRecord(record){
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    transactionObjectStore.add(record)
+}
+
